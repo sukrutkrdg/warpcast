7f43e61fec8e78244e2e04a90e41e68e1ee33322
sukrutkrdg <sukrutkrdg@users.noreply.github.com>
2025-05-27 10:06:41 +0300

    son frame düzeltmeleri 1006

--- api/frame.js
+++ api/frame.js
@@ -1,15 +1,15 @@
-export default function handler(req, res) {
-    if (req.method === 'POST') {
-      return res.json({
-        message: "Button clicked!",
-        // örnek response yapısı
-        frame: {
-          image: "https://warpcast-one.vercel.app/frame.png",
-          button: [{ label: "Geri Dön" }],
-        },
-      });
-    } else {
-      res.status(405).json({ error: "Method not allowed" });
+export default async function handler(req, res) {
+    if (req.method !== 'POST') {
+      return res.status(405).json({ error: 'Method not allowed' });
     }
+  
+    res.setHeader("Content-Type", "application/json");
+    return res.status(200).json({
+      image: "https://warpcast-one.vercel.app/frame.png",
+      post_url: "https://warpcast-one.vercel.app/api/frame",
+      buttons: [
+        { label: "Butona Tıkla" }
+      ]
+    });
   }
   
