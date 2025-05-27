2adb23beec441912862611a00ea9340d946a5791
sukrutkrdg <sukrutkrdg@users.noreply.github.com>
2025-05-27 10:16:07 +0300

    son guncelleme 1016

--- api/frame.js
+++ api/frame.js
@@ -1,15 +1,13 @@
 export default async function handler(req, res) {
-    if (req.method !== 'POST') {
-      return res.status(405).json({ error: 'Method not allowed' });
-    }
-  
-    res.setHeader("Content-Type", "application/json");
-    return res.status(200).json({
-      image: "https://warpcast-one.vercel.app/frame.png",
-      post_url: "https://warpcast-one.vercel.app/api/frame",
-      buttons: [
-        { label: "Butona Tıkla" }
-      ]
-    });
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
   }
-  
+
+  return res.status(200).json({
+    image: "https://warpcast-frame.vercel.app/frame.png",
+    post_url: "https://warpcast-frame.vercel.app/api/frame",
+    buttons: [
+      { label: "Tekrar Tıkla" }
+    ]
+  });
+}
